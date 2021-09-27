ee81935b157b032b3e04fa657e5646f9cbe92ca7
EUNJI YANG <yangej087@gmail.com>
2021-09-27 11:22:49 +0900

    미니톡 commit init

---
+++ lib/views/signin.dart
@@ -0,0 +1,212 @@
+import 'package:chatapp/helper/helperfunctions.dart';
+import 'package:chatapp/helper/theme.dart';
+import 'package:chatapp/services/auth.dart';
+import 'package:chatapp/services/database.dart';
+import 'package:chatapp/views/ChatRoom.dart';
+import 'package:chatapp/views/forgot_password.dart';
+import 'package:chatapp/widget/widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  final Function toggleView;
+
+  SignIn(this.toggleView);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  TextEditingController emailEditingController = new TextEditingController();
+  TextEditingController passwordEditingController = new TextEditingController();
+
+  AuthService authService = new AuthService();
+
+  final formKey = GlobalKey<FormState>();
+
+  bool isLoading = false;
+
+  signIn() async {
+    if (formKey.currentState.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+
+      await authService
+          .signInWithEmailAndPassword(
+              emailEditingController.text, passwordEditingController.text)
+          .then((result) async {
+        if (result != null)  {
+          QuerySnapshot userInfoSnapshot =
+              await DatabaseMethods().getUserInfo(emailEditingController.text);
+
+          HelperFunctions.saveUserLoggedInSharedPreference(true);
+          HelperFunctions.saveUserNameSharedPreference(
+              userInfoSnapshot.documents[0].data["name"]);
+          HelperFunctions.saveUserEmailSharedPreference(
+              userInfoSnapshot.documents[0].data["email"]);
+
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => ChatRoom()));
+        } else {
+          setState(() {
+            isLoading = false;
+            //show snackbar
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: isLoading
+          ? Container(
+              child: Center(child: CircularProgressIndicator()),
+            )
+          : Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/login_2.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  Spacer(),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val)
+                                ? null
+                                : "이메일을 정확하게 입력하여 주세요.";
+                          },
+                          controller: emailEditingController,
+                          style: simpleTextStyle(),
+                          decoration: textFieldInputDecoration("email"),
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          validator: (val) {
+                            return val.length >= 6
+                                ? null
+                                : "비밀번호는 6자리이상 입력하여 주세요.";
+                          },
+                          style: simpleTextStyle(),
+                          controller: passwordEditingController,
+                          decoration: textFieldInputDecoration("password"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ForgotPassword()));
+                        },
+                        child: Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            child: Text(
+                              "비밀번호 찾기",
+                              style: simpleTextStyle(),
+                            )),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      signIn();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      decoration: BoxDecoration( //boxdecoration->버튼으로만들기
+                          borderRadius: BorderRadius.circular(30), //버튼동그랗게
+                          gradient: LinearGradient(
+                            colors: [
+                              const Color(0xff007EF4),
+                              const Color(0xff2A75BC)
+                            ],
+                          )),
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        "로그인",
+                        style: biggerTextStyle(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      widget.toggleView();
+                    },
+                    child:Container(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white),
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        "회원가입",
+                        style:
+                            TextStyle(fontSize: 17, color: CustomTheme.textColor),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: [
+                  //     Text(
+                  //       "Don't have account? ",
+                  //       style: simpleTextStyle(),
+                  //     ),
+                  //     GestureDetector(
+                  //       onTap: () {
+                  //         widget.toggleView();
+                  //       },
+                  //       child: Text(
+                  //         "Register now",
+                  //         style: TextStyle(
+                  //             color: Colors.white,
+                  //             fontSize: 16,
+                  //             decoration: TextDecoration.underline),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                  SizedBox(
+                    height: 50,
+                  )
+                ],
+              ),
+            ),
+    );
+  }
+}
